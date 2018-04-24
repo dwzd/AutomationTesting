b4a28e331f6dec037da0eaf4c3b5c1d258d92fed
dwzd <ddwzdd@msn.com>
2018-04-24 17:50:32 -0400

    edge drivcer

---
+++ automation-testing-master/src/test/java/com/ecvictor/selenium/junit/classic/DesiredCapabilitiesDemo.java
@@ -0,0 +1,21 @@
+package com.ecvictor.selenium.junit.classic;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DesiredCapabilitiesDemo {
+    public static void main(String[] args) {
+        WebDriver driver;
+        String baseUrl = "https://www.bing.com";
+        DesiredCapabilities caps = DesiredCapabilities.edge();
+        caps.setBrowserName("edge");
+        caps.setPlatform(Platform.WIN10);
+        driver = new EdgeDriver();
+        //driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+
+    }
+}
